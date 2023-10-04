ffec89fd818f8c1b1cb4ca0701bc404aca73d0ce
Angellopp <angellojamir@hotmail.com>
2023-10-04 14:13:28 -0500

    eliminar

--- Gemfile
+++
@@ -1,8 +0,0 @@
-source 'https://rubygems.org' 
-ruby '2.6.6' 
-gem 'sinatra', '>= 2.0.1'
-
-# Rerun solo para el entorno de desarrollo
-group :development do  
-    gem 'rerun'  
-end
--- app.rb
+++
@@ -1,7 +0,0 @@
-require 'sinatra'  
-
-class MyApp < Sinatra::Base  
-    get '/' do  
-    "<!DOCTYPE html><html><head></head><body><h1>See you later world</h1></body></html>"  
-    end  
-end
